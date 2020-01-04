09dd2fe10d70ee2852f56ef29bc4671e7bb72857
Darrick Lau <dl5215@corona33.doc.ic.ac.uk>
2020-01-04 17:55:29 +0000

    +updated todos +currently depth-first

--- crawler.py
+++ crawler.py
@@ -1,16 +1,17 @@
+import sys
 import requests
 import re
 from urllib.parse import urlparse
 
-# TODO: starting url as command line arg
-# https://dev.to/fprime/how-to-create-a-web-crawler-from-scratch-in-python-2p46
-
+# TODO: Change to breadth-first
+# TODO: 100 unique URLs in a list, stop and print
+# TODO: nice readme and exceptions for CLA
 
 class Crawler:
-
     def __init__(self, url):
         self.starting_url = url
         self.visited = set()
+        self.visited.add(url)
 
     def fetch_html(self, url):
         try:
@@ -20,38 +21,39 @@ class Crawler:
             return ""
         return html.content.decode('latin-1')
 
-    def get_links(self,url):
+    def get_links(self, url):
         html = self.fetch_html(url)
-        #parses link into scheme, netloc and path
-        parsed = urlparse(url) 
+        # parses link into scheme, netloc and path
+        parsed = urlparse(url)
 
-        #fancy string formatting
+        # fancy string formatting (python3 feature)
         base = f"{parsed.scheme}://{parsed.netloc}"
 
-        #regex catch, links is a list of the caught groups
-        links = re.findall( r'<a\s+(?:[^>]*?\s+)?href="([^"]*)"', html)
-        
+        # regex catch, links is a list of the caught groups
+        links = re.findall(r'<a\s+(?:[^>]*?\s+)?href="([^"]*)"', html)
         for i, link in enumerate(links):
             if not urlparse(link).netloc:
                 full_link = base + link
+                full_link = full_link.rstrip(r'/')
                 links[i] = full_link
 
-        #remove email links
+        # remove email links
         return set(filter(lambda x: 'mailto' not in x, links))
 
-    def crawl(self,url):
+    def crawl(self, url):
+
         for link in self.get_links(url):
-            if link in self.visited:
-                continue
-            print(link)
-            #add the link to the list of visited urls
-            self.visited.add(link)
-            #currently depth-first
-            self.crawl(link)
+            if link not in self.visited:
+                print(link)
+                # add the link to the list of visited urls
+                self.visited.add(link)
+                # currently depth-first
+                self.crawl(link)
 
     def start(self):
         self.crawl(self.starting_url)
 
+
 if __name__ == "__main__":
-    spider = Crawler("http://www.reddit.com")
+    spider = Crawler(sys.argv[1])
     spider.start()
